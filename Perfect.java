47fbd0c2541b18974f19f4b8ed174d554f608462
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-07 22:19:48 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -7,17 +7,18 @@ public class Perfect {
 		String num = args[0];
 		String answer = num + " is a perfect number since "+ num + " = 1";
 		int sum = 1;
+		// #feedback: great usage of loop!
 		for (int i = 2; i < n; i++) {
 			if (n%i==0) {
-			answer = answer + " + "+i;
-			sum = sum + i;
+				answer = answer + " + "+i;
+				sum = sum + i;
 			}
 		}
-	if (sum == n) {
-		System.out.println(answer);
-	}
-	else {
-		System.out.println( n + " is not a perfect number");
-	}
+		if (sum == n) {
+			System.out.println(answer);
+		}
+		else {
+			System.out.println( n + " is not a perfect number");
+		}
 }
 }
